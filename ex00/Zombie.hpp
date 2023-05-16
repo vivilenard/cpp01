2e0423242b796d8474fb2be93199e0e4f31c2c9b
Vivien <Lenard@karls-mbp.speedportneo090128260080>
2023-05-16 23:24:18 +0200

    soso

---
+++ ex00/Zombie.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vlenard <vlenard@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/16 23:06:11 by vlenard           #+#    #+#             */
+/*   Updated: 2023/05/16 23:20:25 by vlenard          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+
+# define ZOMBIE_HPP
+
+#include <iostream>
+
+class Zombie
+{
+    private:
+        std::string _name;
+        
+    public:
+        std::string     getName(void) { return _name;};
+        void            announce (void);
+        Zombie(std::string _name);
+        ~Zombie();
+        
+};
+
+void randomChump( std::string name );
+
+# endif
