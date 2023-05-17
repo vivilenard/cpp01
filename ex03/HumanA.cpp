b9d339faa34853b659009073cd498333045a5bfb
VIVI <vivienlenard00@gmail.com>
2023-05-17 21:34:18 +0200

    gut

--- ex03/HumanA.cpp
+++ ex03/HumanA.cpp
@@ -1,7 +1,6 @@
 #include "HumanA.hpp"
 
-HumanA::HumanA(std::string name, Weapon& weapon): _name(name), _weapon(weapon)
-{}
+HumanA::HumanA(std::string name, Weapon& weapon): _weapon(weapon), _name(name){}
 
 void HumanA::attack ( void )
 {
