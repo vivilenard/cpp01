41271b9015ecaf24d692b88170df87081be989a6
VIVI <vivienlenard00@gmail.com>
2023-05-20 16:28:59 +0200

    ready

--- ex00/Zombie.cpp
+++ ex00/Zombie.cpp
@@ -6,7 +6,7 @@
 /*   By: vlenard <vlenard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 23:10:24 by vlenard           #+#    #+#             */
-/*   Updated: 2023/05/17 10:13:34 by vlenard          ###   ########.fr       */
+/*   Updated: 2023/05/20 16:08:02 by vlenard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,10 @@ Zombie::Zombie (std::string name):_name(name){ }
 
 Zombie::~Zombie()
 {
-    std::cout << _name << " died." << std::endl;
+	std::cout << _name << " died." << std::endl;
 }
 
 void Zombie::announce()
 {
-    std::cout << _name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+	std::cout << _name << ": BraiiiiiiinnnzzzZ..." << std::endl;
 }
